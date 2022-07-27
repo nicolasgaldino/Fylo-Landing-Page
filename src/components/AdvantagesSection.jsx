f266553d79538ac4634b7ab9105bb04cace9f769
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-27 11:55:45 -0300

    Mudança na cor de fundo da página, e alteração na ordem de alguns components.

--- src/components/AdvantagesSection.jsx
+++ src/components/AdvantagesSection.jsx
@@ -10,16 +10,16 @@ const AdvantagesSection = () => {
     <section className={styles.mainAdvantagesContainer}>
       <div className={styles.frstContainer}>
         <AdvantagesCard
-          icon={AccessAnywhere}
-          alt="Access your files, anywhere"
-          title="Access your files, anywhere"
-          description="The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
-        />
-        <AdvantagesCard
           icon={Collaboration}
           alt="Real-time collaboration"
           title="Real-time collaboration"
           description="Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required."
+        />
+        <AdvantagesCard
+          icon={AnyFile}
+          alt="Store any type of file"
+          title="Store any type of file"
+          description="Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
         />
       </div>
       <div className={styles.scndContainer}>
@@ -30,10 +30,10 @@ const AdvantagesSection = () => {
           description="2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
         />
         <AdvantagesCard
-          icon={AnyFile}
-          alt="Store any type of file"
-          title="Store any type of file"
-          description="Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
+          icon={AccessAnywhere}
+          alt="Access your files, anywhere"
+          title="Access your files, anywhere"
+          description="The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
         />
       </div>
     </section>
